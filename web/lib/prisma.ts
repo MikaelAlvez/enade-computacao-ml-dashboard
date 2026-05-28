144b7d3ea7d42fabc919694605bbcb0b113d81b6
Mikael Alves <JOSE.ALVES08240@alunos.ufersa.edu.br>
2026-05-27 21:48:16 -0300

    feat: integra Prisma Accelerate para deploy no Vercel

--- web/lib/prisma.ts
+++ web/lib/prisma.ts
@@ -1,5 +1,11 @@
 import { PrismaClient } from "@/app/generated/prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const globalForPrisma = globalThis as unknown as { prisma: ReturnType<typeof createPrisma> | undefined };
+
+function createPrisma() {
+  return new PrismaClient().$extends(withAccelerate());
+}
+
+export const prisma = globalForPrisma.prisma ?? createPrisma();
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
